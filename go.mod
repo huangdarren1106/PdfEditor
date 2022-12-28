6e7f1428c47a0b177ab577d2f198f0fc151d6bb6
huangdarren1106 <huangdarren1106@gmail.com>
2022-12-28 14:15:33 -0800

    implemented go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/huangdarren1106/PdfEditor
+
+go 1.16
+
+require github.com/fastrodev/fastrex v0.0.0-20211105041338-1516bd717674 // indirect
--- web/modules/root/src/App.js
+++ web/modules/root/src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg'
 import './App.css'
-import { Header } from "@fstr/components"
 
 function App() {
   return (
     <div className="App">
-      <Header text="from root" />
+      hello world
     </div>
   )
 }
